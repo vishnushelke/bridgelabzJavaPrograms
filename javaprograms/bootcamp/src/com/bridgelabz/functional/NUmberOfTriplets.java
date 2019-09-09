0a1039e545a7c74341192e71aa086b797dbac46a
Sanket Sontakke <sanket.sontake041@gmail.com>
2019-09-09 20:18:53 +0530

    last

---
+++ javaprograms/bootcamp/src/com/bridgelabz/functional/NUmberOfTriplets.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.functional;
+import java.util.*;
+public class NUmberOfTriplets {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length of array");
+		int n=sc.nextInt();
+		int count=0;
+		int[] a=new int[n];
+		System.out.println("Enter "+n+" numbers one by one");
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		
+		for(int i=0;i<n-2;i++)
+		{
+			for(int j=i+1;j<i-1;j++)
+			{
+				for(int k=j+1;k<i-2;k++)
+				{
+					if(a[i]+a[j]+a[k]==0)
+					{
+						System.out.println(a[i]+" "+a[j]+" "+a[k]);
+						count++;
+					}
+				}
+			}
+		}
+		System.out.println("NUmber of triplets are "+count);
+
+	}
+
+}
