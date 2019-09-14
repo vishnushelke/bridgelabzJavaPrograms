030c430731c04f15b7b101592e2a98175512dab2
vishnu shelke <shelkeva@gmail.com>
2019-09-14 10:44:01 +0530

    Palindrome checker is done using linkedlist

--- javaprograms/bootcamp/src/com/bridgelabz/data_structure/P5PalindromeChecker.java
+++ javaprograms/bootcamp/src/com/bridgelabz/data_structure/P5PalindromeChecker.java
@@ -1,25 +1,59 @@
+/******************************************************************************
+ *  Compilation:  javac -d bin P5PalindromeChecker.java
+ *  Execution:    java -cp bin com.bridgelabz.algorithm.P5PalindromeChecker n
+ *  
+ *  Purpose: Checks whether given string is palindrome or not.
+ *
+ *  @author  Vishnu Shelke
+ *  @version 1.0
+ *  @since   14-09-2019
+ *
+ ******************************************************************************/
 package com.bridgelabz.data_structure;
 import java.util.*;
 public class P5PalindromeChecker {
 
 	public static void main(String[] args) {
-		Deque<Character> d=new Deque<Character>();
+		LinkedList<Character> list=new LinkedList<Character>();
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the String to be checked");
 		String s=sc.nextLine();
 		String front="",back="";
 		int n=s.length();
 		for (int i = 0; i < s.length(); i++) {
-			d.insertFromFront(s.charAt(i));
+			list.insert(s.charAt(i));
 		}
-		d.show();
-//		for (int i = 0; i < s.length(); i++) {
-//			front=front+d.deleteFromFront();
-//		}
-//		for (int i = 0; i < s.length(); i++) {
-//			back=back+d.deleteFromRear();
-//		}
-//		System.out.println(front+" "+back);
+		
+		Node<Character> node=new Node<Character>();
+		Node<Character> last=list.head;
+		
+		
+		for (int i = 0; i < s.length(); i++) {
+			last=list.head;
+			int j=0;
+			while(j<n-1) 
+			{
+				last=last.next;
+				j++;
+			}
+			back=back+last.data;
+			n--;
+			j=0;
+		}
+		Node<Character> first=list.head;
+		for (int i = 0; i < s.length(); i++) {
+			front=front+first.data;
+			first=first.next;
+		}
+		if(front.equals(back))
+		{
+			System.out.println("String is a Palindrome");
+		}
+		else
+		{
+			System.out.println("String is not Palindrome");
+		}
+		
 	}
 
 }
