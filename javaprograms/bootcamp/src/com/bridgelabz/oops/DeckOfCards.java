1900e1ae8c22f8eac7ca8b833b1943b312d519a4
vishnu shelke <shelkeva@gmail.com>
2019-09-21 15:33:21 +0530

    deck of cards

--- javaprograms/bootcamp/src/com/bridgelabz/oops/DeckOfCards.java
+++ javaprograms/bootcamp/src/com/bridgelabz/oops/DeckOfCards.java
@@ -2,15 +2,20 @@ package com.bridgelabz.oops;
 
 import java.util.*;
 
+import com.bridgelabz.utility.OopsUtility;
+
 public class DeckOfCards {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
 		String[] suit = { "club ", "diamond ", "heart ", "spades " };
-		String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
+		//jack=11,queen=12,king=13,ace=14
+		String[] rank = { "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14" };
 		String[] deck = new String[52];
 		int length = deck.length;
-		int i=0;
+		int i = 0;
+		
+		//created Deck of cards
 		while (length > i) {
 			for (int l = 0; l < 4; l++) {
 				for (int l2 = 0; l2 < 13; l2++) {
@@ -23,17 +28,69 @@ public class DeckOfCards {
 		}
 
 //		for (int l = 0; l < deck.length; l++) {
-//			System.out.print(deck[l] + " ");
+//			System.out.print(deck[l] + ",");
 //		}
+		
+		//shuffled using random function
 		for (int l = 0; l < deck.length; l++) {
-			int randomNumber=rand.nextInt(52);
-			deck[randomNumber]=deck[51-randomNumber];
+			int randomNumber = rand.nextInt(52);
+			deck[randomNumber] = deck[51 - randomNumber];
 		}
-		for (int l = 0; l < deck.length; l++) {
-			System.out.print(deck[l] + " ");
+//		for (int l = 0; l < deck.length; l++) {
+//			System.out.print(deck[l] + ",");
+//		}
+
+		// giving each player 9 cards
+		String[] player1 = new String[9];
+		String[] player2 = new String[9];
+		String[] player3 = new String[9];
+		String[] player4 = new String[9];
+		i = 0;
+		for (int j = 0; j < 9; j++) {
+			player1[j]=deck[j];
+		}
+		for (int j = 0; j < 9; j++) {
+			player2[j]=deck[j+9];
+		}
+		for (int j = 0; j < 9; j++) {
+			player3[j]=deck[j+18];
+		}
+		for (int j = 0; j < 9; j++) {
+			player4[j]=deck[j+27];
 		}
 		
-		//giving each player 9 cards
+		//printing their cards before sorting
+		System.out.println("cards of player1 are :");
+		OopsUtility.showCards(player1);
+		System.out.println();
+		System.out.println("cards of player2 are :");
+		OopsUtility.showCards(player2);
+		System.out.println();
+		System.out.println("cards of player3 are :");
+		OopsUtility.showCards(player3);
+		System.out.println();
+		System.out.println("cards of player4 are :");
+		OopsUtility.showCards(player4);
+		System.out.println();
+		
+		//Sorting and showing cards of each player
+		System.out.println("cards of player1 are after sorting are :");
+		OopsUtility.sortCards(player1);
+		OopsUtility.showCards(player1);
+		System.out.println();
+		System.out.println("cards of player2 are after sorting are :");
+		OopsUtility.sortCards(player2);
+		OopsUtility.showCards(player2);
+		System.out.println();
+		System.out.println("cards of player3 are after sorting are :");
+		OopsUtility.sortCards(player3);
+		OopsUtility.showCards(player3);
+		System.out.println();
+		System.out.println("cards of player4 are after sorting are :");
+		OopsUtility.sortCards(player4);
+		OopsUtility.showCards(player4);
+		
+		
 	}
 
 }
--- javaprograms/bootcamp/src/com/bridgelabz/utility/OopsUtility.java
+++ javaprograms/bootcamp/src/com/bridgelabz/utility/OopsUtility.java
@@ -1,5 +1,39 @@
 package com.bridgelabz.utility;
 
 public class OopsUtility {
+	public static String[] sortCards(String[] player)
+	{
+		String temp="";
+		for (int j = 0; j < player.length; j++) {
+			for (int j2 = 0; j2 < player.length; j2++) {
+				int len1=player[j].length()-1;
+				int len2=player[j2].length()-1;
+				int n1=player[j].charAt(len1)+(player[j].charAt(len1-1))*10;
+				int n2=player[j2].charAt(len2)+(player[j2].charAt(len2-1))*10;
+				if(n1>n2)
+				{
+					temp=player[j];
+					player[j]=player[j2];
+					player[j2]=temp;
+				}
+				player[j].replace("11", "jack");
+				player[j].replace("12", "queen");
+				player[j].replace("13", "king");
+				player[j].replace("14", "ace");
+				player[j2].replace("11", "jack");
+				player[j2].replace("12", "queen");
+				player[j2].replace("13", "king");
+				player[j2].replace("14", "ace");
+			}
+		}
+		return player;
+	}
+	
+	public static void showCards(String[] player)
+	{
+		for (int i = 0; i < player.length; i++) {
+			System.out.print(player[i]+",");
+		}
+	}
 
 }
