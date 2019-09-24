46c80ad5d55fb17f975557f9e7b4bef00a1431a2
vishnu shelke <shelkeva@gmail.com>
2019-09-24 18:27:45 +0530

    stockaccount completed

--- javaprograms/bootcamp/src/com/bridgelabz/oops/Clinique.java
+++ javaprograms/bootcamp/src/com/bridgelabz/oops/Clinique.java
@@ -2,12 +2,16 @@ package com.bridgelabz.oops;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import com.bridgelabz.model.Appointment;
+import com.bridgelabz.model.AppointmentModel;
 import com.bridgelabz.model.Doctor;
 import com.bridgelabz.model.DoctorsModel;
 import com.bridgelabz.model.Patient;
@@ -17,39 +21,41 @@ import com.bridgelabz.utility.Utility;
 public class Clinique {
 
 	public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException {
-		int[] count=new int[5];
-		ObjectMapper mapper=new ObjectMapper();
-		DoctorsModel modelDoctor=new DoctorsModel();
-		PatientModel modelPatient=new PatientModel();
+		int[] count = new int[5];
+		ObjectMapper mapper = new ObjectMapper();
+		DoctorsModel modelDoctor = new DoctorsModel();
+		PatientModel modelPatient = new PatientModel();
+		AppointmentModel modelAppointment=new AppointmentModel();
+
+		ArrayList<Doctor> doctors = new ArrayList<Doctor>();
+		ArrayList<Patient> patients = new ArrayList<Patient>();
+		ArrayList<Appointment> appointments=new ArrayList<Appointment>();
 		
-		ArrayList<Doctor> doctors=new ArrayList<Doctor>();
-		ArrayList<Patient> patients=new ArrayList<Patient>();
-		
-		//adding doctors to clinique
-		Doctor doctorENT=new Doctor();
+		// adding doctors to clinique
+		Doctor doctorENT = new Doctor();
 		doctorENT.setId("ent");
 		doctorENT.setAvailability("am");
 		doctorENT.setIsfull(false);
 		doctorENT.setName("dr. vishnu");
 		doctorENT.setSpecialization("ent");
 		doctors.add(doctorENT);
-		
-		Doctor doctorHeart=new Doctor();
+
+		Doctor doctorHeart = new Doctor();
 		doctorHeart.setAvailability("am");
 		doctorHeart.setId("heart");
 		doctorHeart.setIsfull(false);
 		doctorHeart.setName("dr. vaishnavi");
 		doctorHeart.setSpecialization("heart");
 		doctors.add(doctorHeart);
-		
-		Doctor doctorHomeopathy=new Doctor();
+
+		Doctor doctorHomeopathy = new Doctor();
 		doctorHomeopathy.setAvailability("am");
 		doctorHomeopathy.setId("homeopathy");
 		doctorHomeopathy.setIsfull(false);
 		doctorHomeopathy.setName("rishi");
 		doctorHomeopathy.setSpecialization("homeopathy");
 		doctors.add(doctorHomeopathy);
-		
+
 		Doctor doctorPhysio = new Doctor();
 		doctorPhysio.setAvailability("am");
 		doctorPhysio.setId("physio");
@@ -57,8 +63,8 @@ public class Clinique {
 		doctorPhysio.setName("abhishek");
 		doctorPhysio.setSpecialization("physio");
 		doctors.add(doctorPhysio);
-		
-		Doctor doctorDentist=new Doctor();
+
+		Doctor doctorDentist = new Doctor();
 		doctorDentist.setAvailability("pm");
 		doctorDentist.setId("dentist");
 		doctorDentist.setIsfull(false);
@@ -66,35 +72,71 @@ public class Clinique {
 		doctorDentist.setSpecialization("dentist");
 		doctors.add(doctorDentist);
 		modelDoctor.setDoctors(doctors);
-		mapper.writeValue(new File("/home/user/Desktop/vishnu/bridgelabzJavaPrograms/javaprograms/bootcamp/Files/json files/doctors.json"), modelDoctor);
-		
-		//taking patients
-		boolean exit=false;
-		//while(!exit)
-		{
-			System.out.println("Next please..");
-			System.out.println("welcome to the clinique,which specialist do you want?");
-			for (int i = 0; i < doctors.size(); i++) {
-				System.out.println((i+1)+". "+doctors.get(i).getSpecialization());
-			}
-			int selectSpecialization=Utility.intScan()-1;
-			System.out.println("when do you want an appointment?am or pm?");
-			String slot=Utility.stringScan();
-			if(!slot.equals(doctors.get(selectSpecialization).getAvailability()))
-			{
-				System.out.println("Doctor is not available in this slot.kindly take another slot");
+		mapper.writeValue(new File(
+				"/home/user/Desktop/vishnu/bridgelabzJavaPrograms/javaprograms/bootcamp/Files/json files/doctors.json"),
+				modelDoctor);
+
+		// taking patients
+		DateTimeFormatter dateformatter=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDateTime now=LocalDateTime.now();
+		System.out.println("Enter number of patients");
+		int n=Utility.intScan();
+		for (int j = 0; j < n; j++) {
+			
 				
-			}
-			else
-			{
-				if(doctors.get(selectSpecialization).getIsfull())
+				System.out.println("welcome to the clinique,which specialist do you want?");
+				for (int i = 0; i < doctors.size(); i++) {
+					System.out.println((i + 1) + ". " + doctors.get(i).getSpecialization());
+				}
+				int selectSpecialization = Utility.intScan() - 1;
+				System.out.println("when do you want an appointment?am or pm?");
+				String slot = Utility.stringScan();
+				while (!slot.equals(doctors.get(selectSpecialization).getAvailability())) {
+					System.out.println("Doctor is not available in this slot.kindly take another slot");
+					slot = Utility.stringScan();
+				}
+
+				if (count[selectSpecialization]>4) {
+					System.out.println(
+							"Today doctor's appointments are full,do you want appointment on tomorrow?\n1.yes\n2.no");
+					if (Utility.intScan() == 2) {
+						
+						System.out.println("Thank you for your valuable time");
+						return;
+					}
+				}
+				System.out.println("Doctor available for this slot is "+doctors.get(selectSpecialization).getName()+" Do you want to confirm your appointment?\n1.yes\n2.no");
+				if(Utility.intScan()==2)
 				{
-					System.out.println("Today doctor's appointments are full,do you want appointment on tomorrow?");
+					System.out.println("thank you for your valuable time");
+					
+					return;
 				}
 				
-			}
+				count[selectSpecialization]++;
+				Patient patient=new Patient();
+				System.out.println("Please give your name");
+				String name=Utility.stringScan();
+				patient.setName(name);
+				System.out.println("enter your age");
+				patient.setAge(Utility.intScan());
+				System.out.println("Enter your mobile number");
+				patient.setMobileNumber(Utility.stringScan());
+				System.out.println("your appointment is fixed with "+doctors.get(selectSpecialization).getName()+" have a good day");
+				patients.add(patient);
+				modelPatient.setPatients(patients);
+				Appointment appointment=new Appointment();
+				
+				appointment.setAppointmentDate(dateformatter.format(now));
+				appointment.setDoctorName(doctors.get(selectSpecialization).getName());
+				appointment.setPatientName(name);
+				appointment.setSlot(slot);
+				appointments.add(appointment);
+				modelAppointment.setAppointments(appointments);
 			
 		}
+		 mapper.writeValue(new File("/home/user/Desktop/vishnu/bridgelabzJavaPrograms/javaprograms/bootcamp/Files/json files/appointments.json"), modelAppointment);
+
 	}
 
 }
--- javaprograms/bootcamp/src/com/bridgelabz/oops/StockAccount.java
+++ javaprograms/bootcamp/src/com/bridgelabz/oops/StockAccount.java
@@ -2,7 +2,10 @@ package com.bridgelabz.oops;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -12,6 +15,8 @@ import com.bridgelabz.model.Company;
 import com.bridgelabz.model.CompanySharesModel;
 import com.bridgelabz.model.CustomerInfo;
 import com.bridgelabz.model.CustomerInfoModel;
+import com.bridgelabz.model.Transaction;
+import com.bridgelabz.model.TransactionModel;
 import com.bridgelabz.utility.OopsUtility;
 import com.bridgelabz.utility.Utility;
 
@@ -24,9 +29,11 @@ public class StockAccount {
 		// impoeting all models
 		CompanySharesModel modelCompanyShares = new CompanySharesModel();
 		CustomerInfoModel modelCustomers = new CustomerInfoModel();
+		TransactionModel modelTransaction = new TransactionModel();
 
 		ArrayList<Company> companies = new ArrayList<Company>();
 		ArrayList<CustomerInfo> customers = new ArrayList<CustomerInfo>();
+		ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
 		// adding 3 companies in arraylist
 		Company company = new Company();
@@ -87,122 +94,143 @@ public class StockAccount {
 		mapper.writeValue(new File(
 				"/home/user/Desktop/vishnu/bridgelabzJavaPrograms/javaprograms/bootcamp/Files/json files/customers.json"),
 				modelCustomers);
-		boolean exit=false;
-		while(!exit)
-		{
+		boolean exit = false;
+		while (!exit) {
 			System.out.println("Welcome to the StockExchange!");
 			System.out.println("What do you want to do?\n1.create new account\n2.buy some shares\n3.sell your shares");
-			int action=Utility.intScan();
-			switch(action)
-			{
-			case 1://create an account
-				CustomerInfo customernew=new CustomerInfo();
-				customernew=OopsUtility.createAccount();
+			int action = Utility.intScan();
+			switch (action) {
+			case 1:// create an account
+				CustomerInfo customernew = new CustomerInfo();
+				customernew = OopsUtility.createAccount();
 				customers.add(customernew);
 				System.out.println("customer suuccessfully added!");
 				System.out.println("1.exit\n2.Be with us");
-				if(Utility.intScan()==1)
-				{
-					exit=true;
+				if (Utility.intScan() == 1) {
+					exit = true;
 				}
 				break;
-			case 2://buy shares
+			case 2:// buy shares
 				System.out.println("validate yourself");
 				for (int i = 0; i < customers.size(); i++) {
-					System.out.println((i+1)+". "+customers.get(i).getName());
+					System.out.println((i + 1) + ". " + customers.get(i).getName());
 				}
-				int customerId=Utility.intScan();
-				customer=customers.get(customerId-1);
-				System.out.println("you have "+customer.getRupeesavailable()+" rupees");
+				int customerId = Utility.intScan();
+				customer = customers.get(customerId - 1);
+				System.out.println("you have " + customer.getRupeesavailable() + " rupees");
 				System.out.println("which company shares you want to buy?");
 				for (int i = 0; i < companies.size(); i++) {
 					System.out.println((i + 1) + ". " + companies.get(i).getSymbol());
 				}
 				int response = Utility.intScan();
 				companies.get(response - 1);
-				System.out.println("this company has "+company.getNumberofshares()+" shares and shareprice "+company.getShareprice());
+				System.out.println("this company has " + company.getNumberofshares() + " shares and shareprice "
+						+ company.getShareprice());
 				System.out.println("How many shares you wish to buy?");
 				response = Utility.intScan();
 				int sharesCompany = company.getNumberofshares();
-				while (response > sharesCompany || company.getShareprice()*response>customer.getRupeesavailable()) {
-					if(response > sharesCompany)
-					{
-						System.out.println("you can buy only " + sharesCompany + " shares,enter shares upto this value");
+				while (response > sharesCompany || company.getShareprice() * response > customer.getRupeesavailable()) {
+					if (response > sharesCompany) {
+						System.out
+								.println("you can buy only " + sharesCompany + " shares,enter shares upto this value");
 						response = Utility.intScan();
 					}
-					while(company.getShareprice()*response>customer.getRupeesavailable())
-					{
+					while (company.getShareprice() * response > customer.getRupeesavailable()) {
 						int moneyNeeded = company.getShareprice() * response;
 						while (customer.getRupeesavailable() < moneyNeeded) {
 							System.out.println("You dont have enough money,enter valid number of shares");
 							response = Utility.intScan();
 							moneyNeeded = company.getShareprice() * response;
-					}
-					
+						}
+
 					}
 				}
-				System.out.println("buying "+response+" shares from "+company.getCompanyname()+"...");
+				System.out.println("buying " + response + " shares from " + company.getCompanyname() + "...");
 				System.out.println("Shares bought successfully");
 				company.setNumberofshares(company.getNumberofshares() - response);
-				System.out.println("company remaining shares are "+company.getNumberofshares());
+				System.out.println("company remaining shares are " + company.getNumberofshares());
 				customer.setAvailableshares(customer.getAvailableshares() + response);
-				System.out.println("Your available shares are "+customer.getAvailableshares());
+				System.out.println("Your available shares are " + customer.getAvailableshares());
 				company.setTotalprice(company.getTotalprice() + response * company.getShareprice());
 				customer.setRupeesavailable(customer.getRupeesavailable() - response * company.getShareprice());
-				System.out.println("Money in your account is "+customer.getRupeesavailable());
-				
+				System.out.println("Money in your account is " + customer.getRupeesavailable());
+
 				System.out.println("1.exit\n2.Be with us");
-				if(Utility.intScan()==1)
-				{
-					exit=true;
+				if (Utility.intScan() == 1) {
+					exit = true;
 				}
-		
+
+				// code for saving transactions into transaction array list
+
+				DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+				Date date = new Date();
+				Transaction transaction = new Transaction();
+				transaction.setDate(dateFormat.format(date));
+				transaction.setPaidby(customer.getName());
+				transaction.setPaidto(company.getCompanyname());
+				transaction.setPaidmoney(company.getShareprice()*response);
+				transaction.setStocksold(response);
+				transactions.add(transaction);
+
 				break;
-			case 3://sell shares
+			case 3:// sell shares
 				System.out.println("validate yourself");
 				for (int i = 0; i < customers.size(); i++) {
-					System.out.println((i+1)+". "+customers.get(i).getName());
+					System.out.println((i + 1) + ". " + customers.get(i).getName());
 				}
-				customerId=Utility.intScan();
-				customer=customers.get(customerId-1);
-				System.out.println("you have "+customer.getAvailableshares()+" shares");
+				customerId = Utility.intScan();
+				customer = customers.get(customerId - 1);
+				System.out.println("you have " + customer.getAvailableshares() + " shares");
 				System.out.println("select the companyshares you wish to sell");
 				for (int i = 0; i < companies.size(); i++) {
-					System.out.println((i+1)+". "+companies.get(i).getCompanyname());
+					System.out.println((i + 1) + ". " + companies.get(i).getCompanyname());
 				}
-				int selectCompany=Utility.intScan();
-				company=companies.get(selectCompany-1);
-				System.out.println("this company sharePrice is "+company.getShareprice());
+				int selectCompany = Utility.intScan();
+				company = companies.get(selectCompany - 1);
+				System.out.println("this company sharePrice is " + company.getShareprice());
 				System.out.println("how many shares you wish to sell?");
-				int shareNumber=Utility.intScan();
-				while(shareNumber>customer.getAvailableshares()) {
-					System.out.println("you have only "+customer.getAvailableshares()+" in your account,enter valid number of shares to be sold");
-					shareNumber=Utility.intScan();
+				int shareNumber = Utility.intScan();
+				while (shareNumber > customer.getAvailableshares()) {
+					System.out.println("you have only " + customer.getAvailableshares()
+							+ " in your account,enter valid number of shares to be sold");
+					shareNumber = Utility.intScan();
 				}
-				System.out.println("Selling "+shareNumber+" shares to "+company.getCompanyname()+" ...");
+				System.out.println("Selling " + shareNumber + " shares to " + company.getCompanyname() + " ...");
 				System.out.println("Shares sold successfully");
 				company.setNumberofshares(company.getNumberofshares() + shareNumber);
-				System.out.println("company remaining shares are "+company.getNumberofshares());
+				System.out.println("company remaining shares are " + company.getNumberofshares());
 				customer.setAvailableshares(customer.getAvailableshares() - shareNumber);
-				System.out.println("Your available shares are "+customer.getAvailableshares());
+				System.out.println("Your available shares are " + customer.getAvailableshares());
 				company.setTotalprice(company.getTotalprice() - shareNumber * company.getShareprice());
 				customer.setRupeesavailable(customer.getRupeesavailable() + shareNumber * company.getShareprice());
-				System.out.println("Money in your account is "+customer.getRupeesavailable());
+				System.out.println("Money in your account is " + customer.getRupeesavailable());
+
+				//transaction details
+				dateFormat = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+				date = new Date();
+				transaction = new Transaction();
+				transaction.setDate(dateFormat.format(date));
+				transaction.setPaidby(customer.getName());
+				transaction.setPaidto(company.getCompanyname());
+				transaction.setPaidmoney(company.getShareprice()*shareNumber);
+				transaction.setStocksold(shareNumber);
+				transactions.add(transaction);
+				
 				
 				System.out.println("1.exit\n2.Be with us");
-				if(Utility.intScan()==1)
-				{
-					exit=true;
+				if (Utility.intScan() == 1) {
+					exit = true;
 				}
 				break;
-				
-				
-				default:System.out.println("enter valid input");
+
+			default:
+				System.out.println("enter valid input");
 			}
 		}
 		System.out.println("Thanks for being with us!Have a good day!");
-		
-		
+		modelTransaction.setTransactions(transactions);
+		mapper.writeValue(new File("/home/user/Desktop/vishnu/bridgelabzJavaPrograms/javaprograms/bootcamp/Files/json files/stocktransactions.json"), modelTransaction);
+
 	}
 
 }
